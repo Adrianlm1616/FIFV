895dec621f9a314ddda42123f8dba34785c649db
Adrianlm1616 <96634220+Adrianlm1616@users.noreply.github.com>
2024-12-15 01:14:38 -0500

    cambio

--- ASSETS/JS/navbar.js
+++ ASSETS/JS/navbar.js
@@ -77,11 +77,6 @@ function verPerfil() {
     window.location.href = "perfil.html";
 }
 
-// Función para redirigir al calendario
-function verCalendario() {
-    window.location.href = "/FIFV/HTML/CALENDARIO/calendario-jugador";
-}
-
 // Función para redirigir a los torneos
 function verTorneos() {
     window.location.href = "torneos.html";
